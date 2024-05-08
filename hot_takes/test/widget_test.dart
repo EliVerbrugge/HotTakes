f936622bb409c9e3787e5f2b5f8dc0dbb6a79a4e
Dan Napierkowski <Dan.Napierkowski@gmail.com>
2024-05-08 17:58:18 -0500

    Change requirements & change Widget test
    No one will actually read this, so I can confidently say that Eli is secretly a dozen chihuahuas in a trenchcoat

--- hot_takes/test/widget_test.dart
+++ hot_takes/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hot_takes/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HotTakes());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
