db0c8ff15b94696cab7605ff58906fe2f80e86b9
Eli Verbrugge <eli.verbrugge@gmail.com>
2024-05-08 16:31:33 -0700

    Getting rid of theme file, was just for testing

--- hot_takes/lib/main.dart
+++ hot_takes/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:hot_takes/pages/leaderboard_page.dart';
 import 'package:hot_takes/pages/sign_in_page.dart';
 import 'package:hot_takes/pages/profile_page.dart';
 import 'package:hot_takes/components/takes_model.dart';
-import 'package:hot_takes/themes/darktheme.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:hot_takes/auth/secrets.dart';
